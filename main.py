231951d2ea6fdc0af6a895122b21696a88728ff6
Neel Adwani <nprosofficial@gmail.com>
2021-06-29 12:09:58 +0000

    Omit

--- main.py
+++ main.py
@@ -16,52 +16,9 @@ def index():
 
 
 # Creating different routes
-@app.route('/second')
+@app.route('/meet')
 def second():
-  return "I'm on a separate route"
-
-
-
-# HTTP Methods
-@app.route('/requesthttp', methods=['GET', 'POST'])
-def requesthttp():
-  if request.method == 'POST':
-    return "Auth here"
-  else:
-    return "Ask for creds here"
-
-
-
-# File Uploads (needs an HTML Form)
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-  if request.method == 'GET':
-    file = request.files['filename']
-    file.save('uploads/upload.txt')
-
-
-
-# Reading Cookies 🍪
-@app.route('/readcookie')
-def readcookie():
-  cookie = request.cookies.get('cookie')
-  return cookie
-
-
-
-# Storing Cookies
-@app.route('/storecookie')
-def storecookie():
-  response = make_response(render_template(index.html))
-  response.set_cookie('cookie', 'whatever')
-  return response
-  
-
-
-# Redirects
-@app.route('/redirect')
-def redirec():
-  return redirect(url_for('index'))
+  return render_template("meet.html")
 
 
 
