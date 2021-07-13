f22085c0af56602dbcfdc99b95f69d5d30406b46
Neel Adwani <nprosofficial@gmail.com>
2021-07-13 05:27:31 +0000

    Changed to Dark Mode by Default

--- static/fancy.js
+++ static/fancy.js
@@ -2,23 +2,6 @@ var i = 0;
 function myFunction() {
   console.log("it works");
   if(i%2==0) {
-    document.body.style.backgroundImage = "url(static/bgdark.png)";
-    document.getElementById("lightdark").style.backgroundImage = "url(static/dark.png)";
-    document.getElementById("heading").style.color = "#ECECEC";
-    document.getElementById("heading4").style.color = "#ECECEC";
-    document.getElementById("heading42").style.color = "#ECECEC";
-    document.getElementById("submit1").style.border = "2px solid #ECECEC";
-    document.getElementById("submit2").style.border = "2px solid #ECECEC";
-    document.getElementById("submit1").style.color = "#ECECEC";
-    document.getElementById("submit2").style.color = "#ECECEC";
-    document.getElementById("input1").style.background = "#2f3763";
-    document.getElementById("input2").style.background = "#2f3763";
-    document.getElementById("input1").style.border = "2px solid #ECECEC";
-    document.getElementById("input2").style.border = "2px solid #ECECEC";
-    document.getElementById("input1").style.color = "#ECECEC";
-    document.getElementById("input2").style.color = "#ECECEC";
-  }
-  else {
     document.body.style.backgroundImage = "url(static/bg2.png)";
     document.getElementById("lightdark").style.backgroundImage = "url(static/light.png)";
     document.getElementById("heading").style.color = "#424242";
@@ -34,6 +17,23 @@ function myFunction() {
     document.getElementById("input2").style.border = "2px solid #424242";
     document.getElementById("input1").style.color = "#424242";
     document.getElementById("input2").style.color = "#424242";
+  }
+  else {
+    document.body.style.backgroundImage = "url(static/bgdark.png)";
+    document.getElementById("lightdark").style.backgroundImage = "url(static/dark.png)";
+    document.getElementById("heading").style.color = "#ECECEC";
+    document.getElementById("heading4").style.color = "#ECECEC";
+    document.getElementById("heading42").style.color = "#ECECEC";
+    document.getElementById("submit1").style.border = "2px solid #ECECEC";
+    document.getElementById("submit2").style.border = "2px solid #ECECEC";
+    document.getElementById("submit1").style.color = "#ECECEC";
+    document.getElementById("submit2").style.color = "#ECECEC";
+    document.getElementById("input1").style.background = "#2f3763";
+    document.getElementById("input2").style.background = "#2f3763";
+    document.getElementById("input1").style.border = "2px solid #ECECEC";
+    document.getElementById("input2").style.border = "2px solid #ECECEC";
+    document.getElementById("input1").style.color = "#ECECEC";
+    document.getElementById("input2").style.color = "#ECECEC";
   }
   i++;
   console.log(i);
